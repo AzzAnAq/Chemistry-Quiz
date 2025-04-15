d360df026daabc7cd534ec65b3303f49e1a3c5e3
AzzAnAq <azzan.qureshi1@education.nsw.gov.au>
2025-04-15 12:10:57 +1000

    Changing to custom tkinter, and changing the quiz style.
    The next few changes are me changing the quiz's style which was also previously changed in canva, the code has also been changed into custom tkinter format. This was so I am able to created curved entry boxes for my options. I had changed this already, and copied it over to another doc, this is why i copied everything over from the doc, this was so I could keep the custom tkinter file while also saving the normal tkinter one.

--- Chemistry quiz.py
+++ Chemistry quiz.py
@@ -1,29 +1,34 @@
-from tkinter import*
+from customtkinter import*
+from PIL import Image, ImageTk
 #---------------Play Button Page---------------#
 def openplayb():
-    playb = Toplevel(pg)
+    playb = CTkToplevel(pg)
     playb.title('GAME Options')
     playb.geometry('500x600')
+    playb.resizable(False, False)
     playb.config(bg='#2C5570')
-    
+    playb.lift()
+    playb.focus_force()
+   
 #---------------MAIN PAGE---------------#
-#window details
-pg=Tk()
-pg.geometry
+# window details
+pg=CTk()
+screen_width = pg.winfo_screenwidth()
+screen_height = pg.winfo_screenheight()
+pg.attributes('-fullscreen', True)
 pg.config(bg='#2C5570')
 pg.title('CHEMISTRY QUIZ')
-#logo image
-image= PhotoImage(file='C:/Users/azzan/OneDrive/Desktop/Python/SOFTWARE_QUIZ/Chemistry Quiz (1).png')
-image_label = Label(pg, image = image,bd=0).pack()
-#---------------------------------------------------can1 = Canvas(pg,bg='blue',height=200,width=200)
-#----------------------------------------------------can1.pack(pady=40)
-#----------------------------------------------------can2 = Canvas(pg,bg='blue',height=90,width=360) 
-#----------------------------------------------------can2.pack(pady=30)
-can2 = Button(pg,bg='blue',text='PLAY',command=openplayb)
-can2.pack(pady=30)
-l1=Label(pg, text= 'Name/Nickname (15 charecters):',font=50,bg='#2C5570',fg='white') 
-l1.pack(pady=5)
-l1=Entry(pg,font=50,bd=15)
-l1.pack()
+# main page image
+image1= CTkImage(Image.open(r"SOFTWARE_QUIZ/Chemistry-Quiz/Chemistry Quiz front page and quiz.png"), size=(screen_width,screen_height))
+image1_label = CTkLabel(pg, image = image1, text='').place(anchor = 'center')
+# play button
+play_image = CTkImage(Image.open(r'SOFTWARE_QUIZ/Chemistry-Quiz/play button.png'), size=(432,126))
+play_button = CTkButton(pg, image= play_image, command = openplayb, text='').place(relx= 0.5, rely=0.59, anchor = 'center')
+# quit button
+quit_image = CTkImage(Image.open(r'SOFTWARE_QUIZ/Chemistry-Quiz/quit button.png'), size=(432,127))
+quit_button = CTkButton(pg, image= quit_image, command = pg.destroy , text='').place(relx= 0.5, rely=0.75, anchor = 'center')
+
+
+
 
 pg.mainloop()
