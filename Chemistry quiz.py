7b5860ae2a727d87d2ee300d60cf1d585c36067f
AzzAnAq <azzan.qureshi1@education.nsw.gov.au>
2025-04-15 23:02:56 +1000

    Changing back to tkinter and compleating options page
    I have scrapped the idea of using  custom-tkinter and have modified my design to fit the entry boxes into the format of the background so I can create the illusion of curved entry boxes. I have fitted all my entry boxes to the design, and added the continue button. After completing this and have begun the function for the continue button, which I will complete later. I haven’t left any new problems with the code up till now, though only the GUI has been made and it still doesn't function properly.

--- Chemistry quiz.py
+++ Chemistry quiz.py
@@ -1,34 +1,58 @@
-from customtkinter import*
-from PIL import Image, ImageTk
+from tkinter import*
+import ctypes
+import tkinter.font as tkFont
 #---------------Play Button Page---------------#
 def openplayb():
-    playb = CTkToplevel(pg)
+    # window details
+    playb = Toplevel(pg)
     playb.title('GAME Options')
-    playb.geometry('500x600')
-    playb.resizable(False, False)
-    playb.config(bg='#2C5570')
+    playb.geometry('750x750')
     playb.lift()
     playb.focus_force()
+    #font creation for options entrys 
+    ctypes.windll.gdi32.AddFontResourceW(r"SOFTWARE_QUIZ/Chemistry-Quiz/Fonts/Hangyaboly.ttf")
+    custom_font = tkFont.Font(family="hangyaboly", size=14)
+    # playb page image
+    playb.optionspg= PhotoImage(file = r'SOFTWARE_QUIZ/Chemistry-Quiz/ChemistryQuiz options page.png')
+    playb_image = Label(playb, image = playb.optionspg, bd=0)
+    playb_image.pack()
+    # nickname entry
+    Enickname = Entry(playb, width = 31, font = custom_font, borderwidth=0, highlightthickness=0)
+    Enickname.place(x=300, y=130.5)
+    # Questions entry
+    Equestions = Entry(playb, width = 17, font = custom_font, borderwidth=0, highlightthickness=0)
+    Equestions.place(x=145, y=352.5)
+    # Range entry
+    Erange = Entry(playb, width = 17, font = custom_font, borderwidth=0, highlightthickness=0)
+    Erange.place(x=433, y=352.5)
+    # Topic entry
+    Etopic = Entry(playb, width = 51, font = custom_font, borderwidth=0, highlightthickness=0)
+    Etopic.place(x=112, y=578)
+    # Continue button
+    playb.continue_image = PhotoImage(file = r'SOFTWARE_QUIZ/Chemistry-Quiz/continue button.png')
+    playb.continue_button = Button(playb, image= playb.continue_image, borderwidth=0, highlightthickness=0 )
+    playb.continue_button.place(relx= 0.5, rely= 0.92, anchor = 'center')#command = continue_function
+
    
 #---------------MAIN PAGE---------------#
 # window details
-pg=CTk()
-screen_width = pg.winfo_screenwidth()
-screen_height = pg.winfo_screenheight()
+pg=Tk()
 pg.attributes('-fullscreen', True)
 pg.config(bg='#2C5570')
 pg.title('CHEMISTRY QUIZ')
 # main page image
-image1= CTkImage(Image.open(r"SOFTWARE_QUIZ/Chemistry-Quiz/Chemistry Quiz front page and quiz.png"), size=(screen_width,screen_height))
-image1_label = CTkLabel(pg, image = image1, text='').place(anchor = 'center')
+image1= PhotoImage(file=r"SOFTWARE_QUIZ/Chemistry-Quiz/Chemistry Quiz front page and quiz.png")
+image1_label = Label(pg, image = image1,bd=0).pack()
 # play button
-play_image = CTkImage(Image.open(r'SOFTWARE_QUIZ/Chemistry-Quiz/play button.png'), size=(432,126))
-play_button = CTkButton(pg, image= play_image, command = openplayb, text='').place(relx= 0.5, rely=0.59, anchor = 'center')
+play_image = PhotoImage(file =r'SOFTWARE_QUIZ/Chemistry-Quiz/play button.png')
+play_button = Button(pg, image= play_image, command = openplayb, borderwidth=0, highlightthickness=0 ).place(relx= 0.5, rely=0.59, anchor = 'center')
 # quit button
-quit_image = CTkImage(Image.open(r'SOFTWARE_QUIZ/Chemistry-Quiz/quit button.png'), size=(432,127))
-quit_button = CTkButton(pg, image= quit_image, command = pg.destroy , text='').place(relx= 0.5, rely=0.75, anchor = 'center')
-
+quit_image = PhotoImage(file = r'SOFTWARE_QUIZ/Chemistry-Quiz/quit button.png')
+quit_button = Button(pg, image= quit_image, command = pg.destroy, borderwidth=0, highlightthickness=0 ).place(relx= 0.5, rely=0.75, anchor = 'center')
 
 
 
 pg.mainloop()
+
+
+
